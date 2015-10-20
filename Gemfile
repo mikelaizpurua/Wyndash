ce0dae699d725f05730e2135ab636fb40637b0cf
Mikel Aizpurua <mikel@aizpurua.me>
2015-10-20 12:48:19 -0400

    last full update;

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'google-api-client'
+
 gem 'httparty'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,15 +2,11 @@ class DashboardController < ApplicationController
 
   def index
     @instagram_feed = HTTParty.get("https://api.instagram.com/v1/users/self/feed?access_token=2241851831.6eddba1.9af8868d838040f69feb686b9950997e")['data']
-    @schedule = HTTParty.get("https://www.googleapis.com/calendar/v3/calendars/ebeb817r11817qqjghdvp6r6is@group.calendar.google.com/events")
-
+    auth = {:username => "wyndashmia", :password => "123123wyndash"}
+    @twitter_feed = HTTParty.get("http://twitter.com/statuses/public_timeline.json",
+                         :basic_auth => auth)
   end
-
 end
-
-
-
-
 
 # HTTParty method for Instagram popular searches
 # HTTParty.get("https://api.instagram.com/v1/media/popular?client_id=3212b480b66b4b318aeade560424d99c")
@@ -19,13 +15,6 @@ end
 # Instagram api
 # access-token=2241851831.6eddba1.9af8868d838040f69feb686b9950997e
 # client_id=6eddba1dfbd449e8a697b121f2f9fe38
-
-# Google api
-# OAuth client
-# Here is your client ID
-# 573104718688-8l36fmf93ga007fuu0qgsiatvlbdklfb.apps.googleusercontent.com
-# Here is your client secret
-# YAVtMUoNyuDBmKOYMaqvTdq_
 
 # Schedule Calendar ID
 # ebeb817r11817qqjghdvp6r6is@group.calendar.google.com
