463da3609c8854a5b2387910cb4da4ffdb85706c
Mikel Aizpurua <mikel@aizpurua.me>
2015-10-20 22:27:36 -0400

    final commit before pitch

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,18 @@ class DashboardController < ApplicationController
 
   def index
     @instagram_feed = HTTParty.get("https://api.instagram.com/v1/users/self/feed?access_token=2241851831.6eddba1.9af8868d838040f69feb686b9950997e")['data']
+
+    @twitter_client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = "4Eh3Z0TdmbcEW4BZ907nmVZrd"
+      config.consumer_secret     = "hvRBh0P7Tz0PVhriUi4oMLI85OIWClrKeDztLkBgVzbU8JeK8f"
+      config.access_token        = "4000017922-6C4jzAmrerzR27NoqsG9oF520MjRHMqVvVw4o6M"
+      config.access_token_secret = "HLh81lf3b7Msl4mXnWTBzVQgvwICkLwLuAP8OE42azCoJ"
+    end
+
   end
 end
+
+
 
 # HTTParty method for Instagram popular searches
 # HTTParty.get("https://api.instagram.com/v1/media/popular?client_id=3212b480b66b4b318aeade560424d99c")
